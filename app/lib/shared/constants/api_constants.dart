c8fc7a0ed12b31145d210285b9f9fb6a75c4e711
cubha <okesh5979@gmail.com>
2026-05-25 22:07:39 +0900

    fix: API 주소 서울 리전 EC2 IP로 변경 (43.201.67.1)

--- app/lib/shared/constants/api_constants.dart
+++ app/lib/shared/constants/api_constants.dart
@@ -7,8 +7,8 @@ class ApiConstants {
     const envUrl = String.fromEnvironment('API_BASE_URL');
     if (envUrl.isNotEmpty) return envUrl;
     return kIsWeb
-        ? 'http://75.101.255.28:3001/api'
-        : 'http://75.101.255.28:3001/api';
+        ? 'http://43.201.67.1:3001/api'
+        : 'http://43.201.67.1:3001/api';
   }
 
   static const Duration connectTimeout = Duration(seconds: 10);
